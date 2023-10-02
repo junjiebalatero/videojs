fc7ff91cdc46ee6c722e0b8cb7bdf44140b21aa0
junjiebalatero <junjiebalatero@gmail.com>
2023-10-03 01:12:59 +0800

    ok

--- app.js
+++
@@ -1,9 +0,0 @@
-angular.module('myApp', [])
-  .controller('VideoController', function($scope) {
-    $scope.videoSource = 'https://chocolate-cooperative-herring-689.mypinata.cloud/ipfs/QmeyxsnCUoypsGdUmgYQLi8RvxESP53EVMMmWWL9S9Nnfd';
-    
-    $scope.changeVideo = function() {
-      // Change the video source when the button is clicked
-      $scope.videoSource = 'https://chocolate-cooperative-herring-689.mypinata.cloud/ipfs/QmZBYhYMGFTCMJsP1v3DdNdTaRxtFJBwJRTnhcHBJUM2iz';
-    };
-  });
